f5d44eceeef931bbfdbebeeb0f24bcbe0e5abc8c
Cameron Ross <elpenguino@gmail.com>
2015-11-24 19:14:18 -0400

    update unit tests

--- source/idstore.d
+++ source/idstore.d
@@ -149,118 +149,82 @@ struct IDStore {
 auto openStore(string path) {
 	return IDStore(path);
 }
-unittest {
-	import std.file;
-	import std.datetime;
-	import std.range : iota, zip;
-	import std.array : array;
-	import std.algorithm : map, reduce, sort, setDifference, equal;
-	import std.stdio : writeln, writefln;
-	import std.conv : text;
-	import std.exception : assertNotThrown, assertThrown;
-	import d2sqlite3 : versionString;
-	enum testFilename = ":memory:"; //in-memory
-	//enum testFilename = "test.db"; //file
-	writefln("Powered by sqlite %s", versionString);
-	scope(exit) if (exists(testFilename)) remove(testFilename);
-	enum count = 1000;
-	enum words1 = iota(0,count).map!text;
-	enum words2 = iota(0,count).map!((a) => "word"~text(a));
-	enum words3 = iota(0,count).map!((a) => "Nonexistant"~text(a));
-	enum words4 = iota(0,count).map!((a) => "extra"~text(a));
-	writeln("Beginning database test");
-	StopWatch timer;
-	long[] times;
-	auto db = openStore(testFilename);
-	foreach (database; db.listDbs)
-		db.deleteDB(database);
-	timer.start();
+version(unittest) {
+	void test(uint testid, string filename) {
+		import std.file : remove, exists;
+		import std.datetime : benchmark, Duration;
+		import std.range : iota, zip, enumerate;
+		import std.array : array;
+		import std.algorithm : map, reduce, sort, setDifference, equal;
+		import std.stdio : writeln, writefln;
+		import std.conv : text, to;
+		import std.exception : assertNotThrown, assertThrown;
+		scope(exit) if (exists(filename)) remove(filename);
+		enum count = 1000;
+		enum words1 = iota(0,count).map!text;
+		enum words2 = iota(0,count).map!((a) => "word"~text(a));
+		enum words3 = iota(0,count).map!((a) => "Nonexistant"~text(a));
+		enum words4 = iota(0,count).map!((a) => "extra"~text(a));
+		writeln("Beginning database test ", testid);
+		auto db = openStore(filename);
 
-	assert(words1 !in db["test"], "Found item in empty database");
+		void test1() {
+			assert(words1 !in db["test"], "Found item in empty database");
+		}
+		void test2() {
+			foreach (word; words1)
+				db["test"] ~= word;
 
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Empty database search completed in %sms", times[$-1]);
-	timer.reset();
-	timer.start();
+			assert(db.listDbs() == ["test"], "Database list missing just-added database");
+			assert(db.listIDs("test") == array(words1), "Missing ids in list");
+		}
+		void test3() {
+			foreach (word; words2)
+				db["test"] ~= word;
+		}
+		void test4() {
+			db["test"] ~= words4;
+		}
 
-	foreach (word; words1)
-		db["test"] ~= word;
+		void test5() {
+			foreach (word1, word2, word3; zip(words1, words2, words3)) {
+				assert(word1  in db["test"], "Missing ID: " ~ word1);
+				assert(word2  in db["test"], "Missing ID: " ~ word2);
+				assert(word3 !in db["test"], "Found ID: " ~ word3);
+			}
+		}
 
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database Insertion 1 (one-by-one) completed in %sms", times[$-1]);
-	assert(db.listDbs() == ["test"], "Database list missing just-added database");
-	assert(db.listIDs("test") == array(words1), "Missing ids in list");
-	timer.reset();
-	timer.start();
-
-	foreach (word; words2)
-		db["test"] ~= word;
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database Insertion 2 (one-by-one 2) completed in %sms", times[$-1]);
-	timer.reset();
-	timer.start();
-
-	db["test"] ~= words4;
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database Insertion 3 (range) completed in %sms", times[$-1]);
-	timer.reset();
-	timer.start();
-
-	foreach (word1, word2, word3; zip(words1, words2, words3)) {
-		assert(word1  in db["test"], "Missing ID: " ~ word1);
-		assert(word2  in db["test"], "Missing ID: " ~ word2);
-		assert(word3 !in db["test"], "Found ID: " ~ word3);
+		void test6() {
+			assertNotThrown(db.optimize(), "Optimization failure");
+		}
+		void test7() {
+			assert(words1 in db["test"], "Missing ID from set1");
+			assert(words2 in db["test"], "Missing ID from set2");
+			assert(words4 in db["test"], "Missing ID from set4");
+		}
+		void test8() {
+			assert(equal(db["test"].contains(words1).array.sort(), words1.array.sort()), "Missing ID from set1");
+			assert(equal(db["test"].contains(words2).array.sort(), words2.array.sort()), "Missing ID from set2");
+			assert(equal(db["test"].contains(words4).array.sort(), words4.array.sort()), "Missing ID from set4");
+		}
+		void test9() {
+			assertNotThrown(db["test"].remove(words1), "Deletion 1 failed");
+			assertNotThrown(db["test"].remove(words2), "Deletion 2 failed");
+			assertNotThrown(db["test"].remove(words3), "Deletion of nonexistant ids failed");
+		}
+		void test10() {
+			assert(words1 !in db["test"], "Deletion failed in words1");
+			assert(words2 !in db["test"], "Deletion failed in words2");
+		}
+		auto times = benchmark!(test1, test2, test3, test4, test5, test6, test7, test8, test9, test10)(1)[].map!(to!Duration);
+		foreach (i, time; times.enumerate(1))
+			writefln("Test %d completed in %s", i, time);
+		writefln("Full test completed in %s on thread %s", times.reduce!((a,b) => a+b), testid);
 	}
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database ID check (one-by-one) completed in %sms", times[$-1]);
-	timer.reset();
-	db.optimize();
-	timer.start();
-
-	assert(words1 in db["test"], "Missing ID from set1");
-	assert(words2 in db["test"], "Missing ID from set2");
-	assert(words4 in db["test"], "Missing ID from set4");
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database ID check (ranges) completed in %sms", times[$-1]);
-	timer.reset();
-	timer.start();
-	assert(equal(db["test"].contains(words1).array.sort(), words1.array.sort()), "Missing ID from set1");
-	assert(equal(db["test"].contains(words2).array.sort(), words2.array.sort()), "Missing ID from set2");
-	assert(equal(db["test"].contains(words4).array.sort(), words4.array.sort()), "Missing ID from set4");
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database ID check (ranges, contains) completed in %sms", times[$-1]);
-	timer.reset();
-	timer.start();
-
-	assertNotThrown(db["test"].remove(words1), "Deletion 1 failed");
-	assertNotThrown(db["test"].remove(words2), "Deletion 2 failed");
-	assertNotThrown(db["test"].remove(words3), "Deletion of nonexistant ids failed");
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database ID deletion completed in %sms", times[$-1]);
-	timer.reset();
-	timer.start();
-
-	assert(words1 !in db["test"], "Deletion failed in words1");
-	assert(words2 !in db["test"], "Deletion failed in words2");
-
-	timer.stop();
-	times ~= timer.peek().msecs;
-	writefln("Database post-deletion ID check completed in %sms", times[$-1]);
-	writefln("Database test completed in %sms", reduce!((a,b) => a+b)(cast(ulong)0, times));
-	db.close();
-	writeln("Database closed");
+}
+unittest {
+	import std.parallelism;
+	auto task1 = task!test(1, ":memory:"); //in-memory test
+	task1.executeInNewThread();
+	task1.yieldForce();
 }
